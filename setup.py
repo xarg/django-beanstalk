0a2b82837beec3ef5f16e3174c10219afa36f49b
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2010-05-07 14:26:19 +0200

    Adding deps and including templates

--- setup.py
+++ setup.py
@@ -9,6 +9,23 @@ setup(
     author_email='alexandru.plugaru@gmail.com',
     url='http://github.com/humanfromearth/django-beanstalk',
     license='GPLv3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
     packages=['django_beanstalk'],
-    install_requires=['beanstalkc'],
+    package_data={'django_beanstalk':['templates/admin/django_beanstalk/beanstalkdaemon/*.html']},
+    requires=[
+        'django (>=1.2)',
+        'beanstalkc'
+    ],
+    zip_safe = True
 )
